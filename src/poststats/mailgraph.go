40475b2cbbbc0286212b8168d70ec08a58047a9c
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-09-22 16:17:44 +0200

    Written function for reading data from CSV file

--- src/poststats/mailgraph.go
+++ src/poststats/mailgraph.go
@@ -7,23 +7,68 @@
 package main
 
 import (
+	"encoding/csv"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io"
 	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
 const VERSION = "0.0.1"
 
 type Data struct {
-	Date  *time.Time
+	Date  time.Time
 	Count int
 	Size  int
 }
 
 func read(input string) []Data {
 	var result []Data
+	var date time.Time
+	var count, size int
+	var err error
 
-	// TODO read file and return result
+	layout := "2006-01-02 15:04:05 -0700 MST"
+
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	csvfile := csv.NewReader(file)
+
+	for {
+		record, err := csvfile.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+
+		if date, err = time.Parse(layout, record[0]); err != nil {
+			log.Fatal("Cannot parse date", err)
+		}
+
+		if count, err = strconv.Atoi(record[1]); err != nil {
+			log.Fatal("Cannot parse counted emails", err)
+		}
+
+		if size, err = strconv.Atoi(record[1]); err != nil {
+			log.Fatal("Cannot parse size emails", err)
+		}
+
+		result = append(result, Data{
+			Date:  date,
+			Count: count,
+			Size:  size,
+		})
+	}
+
 	return result
 }
 
